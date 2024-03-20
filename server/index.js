fc828549d40541828308cd4509dcf06b029d9500
harshalkawadse <harshalkawadse2003@gmail.com>
2024-03-20 12:08:38 +0530

    some change

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 
 app.get("/order/food",(req, res)=>{
     const {menu, price, quantity} = req.query;
